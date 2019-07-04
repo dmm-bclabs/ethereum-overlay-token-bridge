e40bd92a1bd056fa84095edea7b016fa9be42e62
Nagato Kasaki <kasaki-nagato@dmm.com>
2019-07-04 13:55:23 +0900

    connect ropsten contract

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const Web3 = require('web3');
 const request = require('superagent');
 
-const settings = require("./config/setting.json");
+const overlayTokenABI = require('./config/OverlayToken.json');
+const setting = require('./config/setting.json');
+
+const parent = setting['ropsten'].url;
+
+async function main () {
+  // TODO: use wss://
+  const ParentProvider = new Web3(new Web3.providers.HttpProvider('https://' + parent));
+  const OverlayToken = await new ParentProvider.eth.Contract(overlayTokenABI, setting['ropsten'].OverlayTokenAddress);
+
+  var totalSupply = await OverlayToken.methods.totalSupply().call();
+  var owner = await OverlayToken.methods.owner().call();
+  console.log(`TotalSupply: ${totalSupply}`);
+  console.log(`Owner: ${owner}`);
+}
+
+main().catch(console.error);
+
+
