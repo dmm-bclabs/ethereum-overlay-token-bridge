957e16bd8c964c196e923ca802795daaa53e262c
Nagato Kasaki <kasaki-nagato@dmm.com>
2019-07-04 17:55:32 +0900

    use WebSocket provider to watch event

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ const setting = require('./config/setting.json');
 const parent = setting['ropsten'].url;
 
 async function main () {
-  // TODO: use wss://
-  const ParentProvider = new Web3(new Web3.providers.HttpProvider('https://' + parent));
+  const ParentProvider = new Web3(new Web3.providers.WebsocketProvider(parent));
   const OverlayToken = await new ParentProvider.eth.Contract(overlayTokenABI, setting['ropsten'].OverlayTokenAddress);
 
   var totalSupply = await OverlayToken.methods.totalSupply().call();
   var owner = await OverlayToken.methods.owner().call();
   console.log(`TotalSupply: ${totalSupply}`);
   console.log(`Owner: ${owner}`);
+
+  OverlayToken.events.Transfer(function(error, result) {
+    console.log('Watch transfer event');
+    if (!error) {
+      console.log(result);
+    }
+  });
 }
 
 main().catch(console.error);
